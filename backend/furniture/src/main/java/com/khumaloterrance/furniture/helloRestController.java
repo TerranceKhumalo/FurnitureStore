33e3f0c8d3bcd569cf4e8ed34db54bb9f66550c2
TerranceKhumalo <khumaloterrance@gmail.com>
2021-09-08 15:09:39 +0200

    fix folder structure

--- backend/furniture/src/main/java/com/khumaloterrance/furniture/helloRestController.java
+++
@@ -1,2 +0,0 @@
-package com.khumaloterrance.furniture;public class helloRestController {
-}
---
+++ backend/furniture/src/main/java/com/khumaloterrance/furniture/restController/helloRestController.java
@@ -0,0 +1,13 @@
+package com.khumaloterrance.furniture;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloRestController {
+
+    @GetMapping("/home")
+    public String helloWorld(){
+        return "<h1>Hello World</h1>";
+    }
+}
