01e578739422307adf07a011783b29b6f03ab6fc
TerranceKhumalo <khumaloterrance@gmail.com>
2021-09-14 15:46:23 +0200

    add form validation on registration.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterGuard } from './auth/router.guard';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import {LoginPageComponent} from './components/login-page/login-page.component';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent, canActivate: [RouterGuard]},
   {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegistrationPageComponent},
   {path: '**', component: ErrorPageComponent}
   
 ];
--- frontend/src/app/components/registration-page/registration-page.component.ts
+++ frontend/src/app/components/registration-page/registration-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-page',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPageComponent implements OnInit {
 
+  errorMessage: string = 'Invalid username or password';
+  inValidLogin: boolean = false;
+
+  registrationForm = new FormGroup({
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    userEmail: new FormControl('',[
+      Validators.required,
+      Validators.email
+    ]),
+    userPassword: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+    ])
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(){}
+
+  // get userName() { return this.registrationForm.get('userName'); }
 
 }
