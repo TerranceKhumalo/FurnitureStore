b49caed45a8c2d4538c427c9785cab9fbaac0348
Terrance Khumalo <khumaloterrance@gmail.com>
2021-10-11 15:57:39 +0200

    add cart page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ProductsPageComponent } from './components/products-page/products-page.
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
 
 // const oktaConfig = Object.assign({
 //   onAuthRequired: (inject: { get: (arg0: typeof Router) => any; })=>{
@@ -21,6 +22,7 @@ const routes: Routes = [
   {path: 'home', component: HomePageComponent, canActivate: [OktaAuthGuard]},
   {path: '', redirectTo: '/shop', pathMatch: 'full'},
   {path: 'shop', component: ProductsPageComponent},
+  {path: 'cart', component: CartPageComponent},
   {path: 'products/:id', component: ProductDetailComponent},
   {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'login', component: LoginPageComponent},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LoginStatusComponent } from './components/login-status/login-status.com
 import { Router } from '@angular/router';
 import myAppConfig from './config/my-app-config';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
 
 const oktaConfig = Object.assign({
   onAuthRequired: (inject: any)=>{
@@ -40,7 +41,8 @@ const oktaConfig = Object.assign({
     ProductCategoryMenuComponent,
     SearchComponent,
     LoginStatusComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
